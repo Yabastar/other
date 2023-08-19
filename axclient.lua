f42e67f81167de0610525258165d875cc9c132bd
Yabastar <138197982+Yabastar@users.noreply.github.com>
2023-08-18 23:30:29 -0500

    Update axclient.lua

--- axclient.lua
+++ axclient.lua
@@ -1,4 +1,8 @@
 peripheral.find("modem", rednet.open)
-rednet.send(38, "page1")
+print("Server to connect to: ")
+server = read()
+if server == "example" then
+  rednet.send(38, "page1")
+end
 local id, message = rednet.receive()
 print(message)
