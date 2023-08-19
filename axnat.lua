c474f6e06adf71c5cc0626dc893434a4f4b79c28
Yabastar <138197982+Yabastar@users.noreply.github.com>
2023-08-18 23:25:10 -0500

    Update axnat.lua

--- axnat.lua
+++ axnat.lua
@@ -5,7 +5,9 @@ rednet.host("AXNAT", "AXN Server 1")
 while true do
 	sleep(0.1)
 	local id, message = rednet.receive()
+	print("Connection to " .. id)
 	if message == "page1" then
 		rednet.send(id, "Hello from rednet!")
+		print("Client accessed page1")
 	end
 end
